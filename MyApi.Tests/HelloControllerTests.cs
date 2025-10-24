9f8c17f013993f3ffa4d1f01d6661fe3b4ae9de9
Huiyong Huang <ring9834@sina.com>
2025-10-24 22:58:37 +1300

    source code
    source code

---
+++ MyApi.Tests/HelloControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApi.Controllers;
+using Xunit;
+
+namespace MyApi.Tests
+{
+    public class HelloControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsOkWithMessage()
+        {
+            var controller = new HelloController();
+            var result = controller.Get() as OkObjectResult;
+            Assert.NotNull(result);
+            Assert.Equal("Hello from .NET API!", (result.Value as dynamic).message);
+        }
+    }
+}
